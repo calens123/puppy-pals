45c40cbfe23c843f6aec91f6ff7c35832afa2548
Calen Sturtevant <calen.s@icloud.com>
2024-12-03 22:09:16 -0800

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import { puppyList } from "./data.js";
+import { useState } from "react";
 
 function App() {
+  // Initialize state with puppyList
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppies); // Log the puppies array
+
   return (
-    <>
-      <div></div>
-    </>
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
   );
 }
 
